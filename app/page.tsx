3bb7294b4c54a9f3effb63642237d67762d65672
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-12-15 03:18:29 -0300

    Fix: Fixed mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import BarReport from "@/components/BarReport";
-import ConsumptionReport from "@/components/ConsumptionReport";
-import Nav from "@/components/Nav";
-import { PieComponent } from "@/components/PieReport";
-import QuickEnergy from "@/components/QuickEnergy";
-import SideBar from "@/components/SideBar";
+import BarReport from "@/components/charts/BarReport";
+import ConsumptionReport from "@/components/charts/ConsumptionReport";
+import Nav from "@/components/nav/Nav";
+import { PieComponent } from "@/components/charts/PieReport";
+import QuickEnergy from "@/components/charts/QuickEnergy";
+import SideBar from "@/components/nav/SideBar";
 import { BarReportData, ConsumptionData, PieData } from "./data";
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="w-full rounded-lg flex lg:h-[calc(100svh-80px)]">
         <SideBar />
         <div className="lg:flex-[9] w-full lg:w-auto bg-gray-100 flex flex-col lg:grid grid-flow-row grid-rows-2 gap-5 p-5 ">
-          <div className="flex flex-col lg:flex-row w-full justify-center items-center gap-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-row w-full justify-center items-center gap-5">
             <QuickEnergy />
             <ConsumptionReport data={ConsumptionData} />
           </div>
